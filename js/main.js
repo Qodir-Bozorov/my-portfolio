a9a3e0c4193745add0b40197f4fae81835d88be5
Qodir-Bozorov <qodirmac@gmail.com>
2021-04-19 14:09:56 +0500

    header sticky

--- js/main.js
+++ js/main.js
@@ -5,3 +5,20 @@ toggler.addEventListener("click", function (e) {
   e.preventDefault()
   navList.classList.toggle("site-nav__nav-list--open")
 })
+
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("siteHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
